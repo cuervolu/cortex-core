45663e8de4f0a134e86d6efa2ec5f64a9b876bbb
Ángel Cuervo <angel.cuervo187@gmail.com>
2025-09-20 23:18:43 -0300

    fix(frontend): improve layout by adjusting container alignment in Section.vue

--- apps/frontend/src/components/layout/Section.vue
+++ apps/frontend/src/components/layout/Section.vue
@@ -1,15 +1,16 @@
-<script setup lang="ts">
+<script
+    setup
+    lang="ts">
 
 </script>
 
 <template>
   <section class="w-full border-b py-12 md:py-24 lg:py-32">
-    <div class="container px-4 md:px-6">
+    <div class="container mx-auto px-4 md:px-6">
       <slot/>
     </div>
   </section>
 </template>
-
 <style scoped>
 
 </style>
